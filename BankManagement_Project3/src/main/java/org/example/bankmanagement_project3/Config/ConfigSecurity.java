8b4b93d6861b9a167904b0141bfb8016fd5019ad
Reenad Almadhi <ren3011h@gmail.com>
2026-06-10 19:48:40 +0300

    ConfigSecurity.java

--- BankManagement_Project3/src/main/java/org/example/bankmanagement_project3/Config/ConfigSecurity.java
+++ BankManagement_Project3/src/main/java/org/example/bankmanagement_project3/Config/ConfigSecurity.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 public class ConfigSecurity {
@@ -26,28 +25,17 @@ public class ConfigSecurity {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
-                // قبل اكتب اي باث لازم افتح له باث في الكنترولر قبل
                 .authorizeHttpRequests(auth -> auth
-                        // فتح مسار التسجيل للجميع للعملاء والموظفين
-                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register-customer").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register-employee").permitAll()
+                        
+                        .requestMatchers("/api/v1/auth/register-customer", "/api/v1/auth/register-employee").permitAll()
 
-                        // صلاحيات الـ Admin لإدارة المستخدمين والموظفين والاطلاع الكلي
+                        .requestMatchers("/api/v1/account/create", "/api/v1/account/my-accounts", "/api/v1/account/view-my-account/{id}", "/api/v1/account/deposit/**", "/api/v1/account/withdraw/**", "/api/v1/account/transfer/**").hasAuthority("CUSTOMER")
+
+                        .requestMatchers("/api/v1/account/activate/{id}", "/api/v1/account/block/{id}", "/api/v1/account/get-all").hasAnyAuthority("EMPLOYEE", "ADMIN")
+
                         .requestMatchers("/api/v1/auth/get-users").hasAuthority("ADMIN")
 
-                        // صلاحيات الـ Customer لإنشاء حساباتهم والعمليات المالية الخاصة بهم
-                        .requestMatchers("/api/v1/account/create").hasAuthority("CUSTOMER")
-                        .requestMatchers("/api/v1/account/my-accounts").hasAuthority("CUSTOMER")
-                        .requestMatchers("/api/v1/account/deposit/**", "/api/v1/account/withdraw/**").hasAuthority("CUSTOMER")
-                        .requestMatchers("/api/v1/account/transfer/**").hasAuthority("CUSTOMER")
-                        .requestMatchers("/api/v1/account/view-my-account/{id}").hasAuthority("CUSTOMER")
-
-                        // صلاحيات الـ Employee والـ Admin لتفعيل الحسابات أو حظرها والاطلاع العام
-                        .requestMatchers("/api/v1/account/activate/{id}").hasAnyAuthority("EMPLOYEE", "ADMIN")
-                        .requestMatchers("/api/v1/account/block/{id}").hasAnyAuthority("EMPLOYEE", "ADMIN")
-                        .requestMatchers("/api/v1/account/get-all").hasAnyAuthority("EMPLOYEE", "ADMIN")
-
-                        .anyRequest().authenticated() // يعني اي باث مو موجود فوق بالماتشر يسوي لي ان اوثريازد ولا يشغله
+                        .anyRequest().authenticated() 
                 )
                 .logout(logout -> logout
                         .logoutUrl("/api/v1/auth/logout")
@@ -57,5 +45,6 @@ public class ConfigSecurity {
                 .httpBasic(httpBasic -> {});
 
         return http.build();
-    }
+    }}
+
 }
